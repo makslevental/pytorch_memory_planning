9162aeff30ff3cd13c1a64205d6a795519fa2b4e
Maksim <maksim.levental@gmail.com>
2021-12-04 16:17:21 -0600

    ready to run

--- example_models.py
+++ example_models.py
@@ -1,61 +1,15 @@
 import inspect
 
 import torch
-# from torchvision import models
-# from torchvision.models.segmentation import deeplabv3_resnet50
 from torch import nn
-from torch._C._autograd import DeviceType
 from torch.nn import functional as F
-# from torchvision import models
-# from torchvision.models.segmentation import deeplabv3_resnet50, deeplabv3_resnet101
-# from transformers import BertTokenizer, BertModel, BertConfig
-# from torchvision import models
-# from torchvision.models.segmentation import deeplabv3_resnet101
+from torchvision import models
 from transformers import BertTokenizer, BertModel, BertConfig
 
-
-def make_resnets():
-    models_names = ["resnet18", "resnet34", "resnet50", "resnet101", "resnet152"]
-
-    for models_name in models_names:
-        model = getattr(resnet, models_name)(pretrained=False)
-        model.eval()
-        x = torch.rand((1, 3, 224, 244))
-        y = torch.jit.trace(model, (x,), _force_outplace=True)
-        y._c = torch._C._freeze_module(y._c)
-        # print(y.inlined_graph)
-        y.save(f"models/{models_name}.pt")
+from modeling_gpt2 import GPT2LMHeadModel
 
 
-def make_mobilenets():
-    x = torch.rand((1, 3, 224, 244))
-
-    model = models.mobilenet_v2()
-    model.eval()
-    y = torch.jit.trace(model, (x,))
-    y.save(f"models/mobilenet_v2.pt")
-
-    model = models.mobilenet_v3_small()
-    model.eval()
-    y = torch.jit.trace(model, (x,))
-    y.save(f"models/mobilenet_v3_small.pt")
-
-    model = models.mobilenet_v3_large()
-    model.eval()
-    y = torch.jit.trace(model, (x,))
-    y.save(f"models/mobilenet_v3_large.pt")
-
-
-def make_deeplab():
-    x = torch.rand((1, 3, 224, 244))
-
-    model = deeplabv3_resnet101()
-    model.eval()
-    y = torch.jit.trace(model, (x,), strict=False)
-    y.save(f"models/deeplabv3_resnet101.pt")
-
-
-def make_bert_input():
+def make_bert_input(batch_size, hw):
     enc = BertTokenizer.from_pretrained("bert-base-uncased")
 
     # Tokenizing input text
@@ -69,97 +23,102 @@ def make_bert_input():
     segments_ids = [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1]
 
     # Creating a dummy input
-    tokens_tensor = torch.tensor([indexed_tokens])
-    segments_tensors = torch.tensor([segments_ids])
+    tokens_tensor = torch.tensor([indexed_tokens * hw] * batch_size)
+    segments_tensors = torch.tensor([segments_ids * hw] * batch_size)
     return tokens_tensor, segments_tensors
 
 
-def make_bert():
-    config = BertConfig(
-        vocab_size_or_config_json_file=32000,
-        hidden_size=768,
-        num_hidden_layers=12,
-        num_attention_heads=12,
-        intermediate_size=3072,
-        torchscript=True,
-    )
-
-    inp = make_bert_input()
-
-    model = BertModel(config)
-    model.eval()
-    traced_model = torch.jit.trace(model, inp)
-    torch.jit.save(traced_model, "models/large_bert.pt")
+# - gpt2: 12
+# - gpt2-medium: 24
+# - gpt2-large: 36
+# - gpt2-xl: 48
 
 
-def make_transformer():
-    # download model from https://drive.google.com/file/d/1FKNQ4TYBXU5ArhLr3a5szjAymqNz4Gnm/view?usp=sharing
-    model = torch.jit.load("models/attention_is_all_you_need.pt")
+def make_gpt(batch_size, hw):
+    with torch.no_grad():
+        model = GPT2LMHeadModel.from_pretrained("gpt2")
+        tokens_tensor, segments_tensors = make_bert_input(batch_size, hw)
+        traced_model = torch.jit.trace(
+            model, (tokens_tensor, segments_tensors), strict=False, check_trace=False
+        )
+        # torch.jit.save(traced_model, f"models/gpt2.pt")
+        torch.jit.save(traced_model, f"models/gpt2.x{batch_size}.y{hw}.pt")
 
-    froze = model
-    inputs = list(froze.graph.inputs())
-
-    # firs inp is batch size
-    inputs[1].setType(inputs[1].type().with_sizes([None, None]))
-    inputs[2].setType(inputs[1].type())
-
-    for node in froze.graph.findAllNodes("prim::Constant"):
-        ival = node.output().toIValue()
-        if not isinstance(ival, torch.Tensor):
-            continue
-        node.output().inferTypeFrom(ival)
-
-    torch._C._jit_pass_propagate_shapes_on_graph(froze.graph)
-    torch._C._jit_pass_canonicalize_for_shape_analysis(froze.graph)
-    compute = torch._C._jit_pass_propagate_shapes_on_graph_and_build_compute(
-        froze.graph
-    )
-    eval_g = compute.partial_eval_shape_graph()
-    mapping = {
-        "%" + n.debugName(): f"SS({v})"
-        for n, v in compute.graph_output_to_symbolic_shape_dim().items()
-    }
-
-    [node.destroy() for node in eval_g.findAllNodes("prim::RaiseException")]
-    torch._C._jit_pass_dce(eval_g)
-    if True:
-        # slightly cheating a bit here but this value will always be False
-        # or the model throws
-        # https://github.com/jadore801120/attention-is-all-you-need-pytorch/blob/132907dd272e2cc92e3c10e6c4e783a87ff8893d/transformer/Models.py#L45
-        # x.size(1) > pos_table lenght will throw
+        # model = GPT2LMHeadModel.from_pretrained("gpt2-medium")
+        # tokens_tensor, segments_tensors = make_bert_input(batch_size, hw)
+        # traced_model = torch.jit.trace(
+        #     model, (tokens_tensor, segments_tensors), strict=False, check_trace=False
+        # )
+        # # torch.jit.save(traced_model, f"models/gpt2-medium.pt")
+        # torch.jit.save(traced_model, f"models/gpt2-medium.x{batch_size}.y{hw}.pt")
         #
-        # TODO: rely on compiler
-        cons = eval_g.insertConstant(False)
-        ge = eval_g.findNode("aten::ge")
-        ge_inps = list(ge.inputs())
-        assert ge_inps[1].toIValue() == 200
-        assert ge_inps[0].node().kind() == "aten::__getitem__"
-        cons.node().moveBefore(ge)
-        ge.replaceAllUsesWith(cons.node())
-        torch._C._jit_pass_constant_propagation(eval_g)
-
-    eval_g.makeMultiOutputIntoTuple()
-    func = torch._C._create_function_from_graph("partial_eval_graph", eval_g)
-    # print(func.code)
-    # print(eval_g)
-    [node.destroy() for node in eval_g.findAllNodes("prim::RaiseException")]
-    torch._C._jit_pass_dce(eval_g)
-    # print(eval_g)
-    # print(mapping)
-    # TODO: assumption that the input(1) size < 200
-    # gets clean graph
-    return compute, eval_g, mapping, froze
+        # model = GPT2LMHeadModel.from_pretrained("gpt2-large")
+        # tokens_tensor, segments_tensors = make_bert_input(batch_size, hw)
+        # traced_model = torch.jit.trace(
+        #     model, (tokens_tensor, segments_tensors), strict=False, check_trace=False
+        # )
+        # # torch.jit.save(traced_model, f"models/gpt2-large.pt")
+        # torch.jit.save(traced_model, f"models/gpt2-large.x{batch_size}.y{hw}.pt")
 
 
-def make_dcgan():
+def make_bert(batch_size, hw):
+    with torch.no_grad():
+        inp = make_bert_input(batch_size, hw)
+        config = BertConfig(
+            vocab_size_or_config_json_file=32000,
+            hidden_size=768,
+            num_hidden_layers=12,
+            num_attention_heads=12,
+            intermediate_size=3072,
+            torchscript=True,
+        )
+        model = BertModel(config)
+        model.eval()
+        traced_model = torch.jit.trace(model, inp)
+        # torch.jit.save(traced_model, f"models/large_bert.pt")
+        torch.jit.save(traced_model, f"models/large_bert.x{batch_size}.y{hw}.pt")
+
+        inp = make_bert_input(batch_size, hw)
+        config = BertConfig(
+            vocab_size_or_config_json_file=32000 // 2,
+            hidden_size=768 // 2,
+            num_hidden_layers=12 // 2,
+            num_attention_heads=12 // 2,
+            intermediate_size=3072 // 2,
+            torchscript=True,
+        )
+        model = BertModel(config)
+        model.eval()
+        traced_model = torch.jit.trace(model, inp)
+        # torch.jit.save(traced_model, f"models/medium_bert.pt")
+        torch.jit.save(traced_model, f"models/medium_bert.x{batch_size}.y{hw}.pt")
+
+        inp = make_bert_input(batch_size, hw)
+        config = BertConfig(
+            vocab_size_or_config_json_file=32000 // 4,
+            hidden_size=768 // 4,
+            num_hidden_layers=12 // 4,
+            num_attention_heads=12 // 4,
+            intermediate_size=3072 // 4,
+            torchscript=True,
+        )
+        model = BertModel(config)
+        model.eval()
+        traced_model = torch.jit.trace(model, inp)
+        # torch.jit.save(traced_model, f"models/small_bert.pt")
+        torch.jit.save(traced_model, f"models/small_bert.x{batch_size}.y{hw}.pt")
+
+
+def make_dcgan(batch_size, hw):
     model = torch.hub.load("facebookresearch/pytorch_GAN_zoo:hub", "DCGAN")
     gnet = model.getNetD()
-    t = torch.rand((1, 3, 128, 128))
-    traced_model = torch.jit.trace(gnet, [t])
-    torch.jit.save(traced_model, "models/dcgan.pt")
+    t = torch.rand((batch_size, 3, hw, hw))
+    traced_model = torch.jit.trace(gnet, (t,))
+    torch.jit.save(traced_model, f"models/dcgan.pt")
+    # torch.jit.save(traced_model, f"models/dcgan.x{batch_size}.y{hw}.pt")
 
 
-def make_unet():
+def make_unet(batch_size, hw):
     model = torch.hub.load(
         "mateuszbuda/brain-segmentation-pytorch",
         "unet",
@@ -169,9 +128,9 @@ def make_unet():
         pretrained=True,
     )
     model.eval()
-    x = torch.rand((1, 3, 256, 256))
+    x = torch.rand((batch_size, 3, hw, hw))
     y = torch.jit.trace(model, (x,))
-    y.save(f"models/unet.pt")
+    y.save(f"models/unet.x{batch_size}.y{hw}.pt")
 
 
 def _vision_models():
@@ -209,53 +168,71 @@ class Net(nn.Module):
         return x
 
 
-def make_toy_model():
-    x = torch.rand((1, 3, 32, 32))
+def make_toy_model(batch_size, hw):
+    x = torch.rand((batch_size, 1, hw, hw))
 
     model = Net()
     model.eval()
     y = torch.jit.trace(model, (x,), strict=False)
+    # y.save(f"models/toy_model.x{batch_size}.y{hw}.pt")
     y.save(f"models/toy_model.pt")
 
 
-# def make_all_vision_models():
-#     modelss = _vision_models()
-#     for name, model_fn in modelss.items():
-#         print(name)
-#         with torch.no_grad():
-#             model = model_fn(pretrained=False).eval()
-#             print(model)
-#             x = torch.rand((1, 3, 52, 52))
-#             y = torch.jit.trace(model, (x,), strict=False)
-
-
-def make_vision_model(model_name):
+def make_vision_model(model_name, batch_size=1, hw=64):
+    if "inception" in model_name:
+        hw = 128
     with torch.no_grad():
         model = vision_models(model_name).eval()
-        x = torch.rand((1, 3, 64, 44))
+        x = torch.rand((batch_size, 3, hw, hw))
         y = torch.jit.trace(model, (x,), strict=False)
-        y.save(f"models/{model_name}.pt")
+
+    y.save(f"models/{model_name}.pt")
 
 
 import multiprocessing
 
 
-def make_all_vision_models():
+def make_all_vision_models(batch_size, hw):
     modelss = _vision_models()
     with multiprocessing.Pool(multiprocessing.cpu_count()) as pool:
         for name, model_fn in modelss.items():
             print(name)
-            pool.apply_async(make_vision_model, (name,))
+            pool.apply_async(make_vision_model, (name, batch_size, hw))
         pool.close()
         pool.join()
 
 
+def make_transformers():
+    with multiprocessing.Pool(multiprocessing.cpu_count()) as pool:
+        for batch_size in [1, 2, 4, 8, 16, 32, 64]:
+            for hw in [1, 2, 4, 8]:
+                pool.apply_async(make_bert, (batch_size, hw))
+                # pool.apply_async(make_gpt, (batch_size, hw))
+        pool.close()
+        pool.join()
+
+
+def test_bert_jit_model():
+    with torch.no_grad():
+        inps = make_bert_input(5, 5)
+        print(inps)
+        model = torch.jit.load("/home/mlevental/dev_projects/pytorch_memory_planning/models/small_bert.pt")
+        print(model.code)
+        print(model.graph)
+        # out = model(*inps)
+    # print(out)
+
 if __name__ == "__main__":
+    # make_transformers()
     # pass
     # r = resnet18()
     # print(thnn_conv2d)
-    # make_transformer()
-    make_toy_model()
+    # make_dcgan(1, 64)
+    # make_transformers()
+    make_all_vision_models(1, 32)
+    # make_vision_model("inception_v3", 1, 32)
+
+    # make_toy_model()
 #
 #     def _conv_forward(self, input: Tensor, weight: Tensor, bias: Optional[Tensor]):
 # # (Tensor self, Tensor weight, int[2] kernel_size, Tensor? bias, int[2] stride, int[2] padding) -> (Tensor output, Tensor finput)
@@ -271,3 +248,62 @@ if __name__ == "__main__":
 #         return torch._C._nn._slow_conv2d_forward(input, weight, self.kernel_size, bias, self.stride,
 #                         self.padding)[0]
 #
+
+
+# def make_transformer():
+#     # download model from https://drive.google.com/file/d/1FKNQ4TYBXU5ArhLr3a5szjAymqNz4Gnm/view?usp=sharing
+#     model = torch.jit.load(f"models/attention_is_all_you_need.x{batch_size}.y{hw}.pt")
+#
+#     froze = model
+#     inputs = list(froze.graph.inputs())
+#
+#     # firs inp is batch size
+#     inputs[1].setType(inputs[1].type().with_sizes([None, None]))
+#     inputs[2].setType(inputs[1].type())
+#
+#     for node in froze.graph.findAllNodes("prim::Constant"):
+#         ival = node.output().toIValue()
+#         if not isinstance(ival, torch.Tensor):
+#             continue
+#         node.output().inferTypeFrom(ival)
+#
+#     torch._C._jit_pass_propagate_shapes_on_graph(froze.graph)
+#     torch._C._jit_pass_canonicalize_for_shape_analysis(froze.graph)
+#     compute = torch._C._jit_pass_propagate_shapes_on_graph_and_build_compute(
+#         froze.graph
+#     )
+#     eval_g = compute.partial_eval_shape_graph()
+#     mapping = {
+#         "%" + n.debugName(): f"SS({v})"
+#         for n, v in compute.graph_output_to_symbolic_shape_dim().items()
+#     }
+#
+#     [node.destroy() for node in eval_g.findAllNodes("prim::RaiseException")]
+#     torch._C._jit_pass_dce(eval_g)
+#     if True:
+#         # slightly cheating a bit here but this value will always be False
+#         # or the model throws
+#         # https://github.com/jadore801120/attention-is-all-you-need-pytorch/blob/132907dd272e2cc92e3c10e6c4e783a87ff8893d/transformer/Models.py#L45
+#         # x.size(1) > pos_table lenght will throw
+#         #
+#         # TODO: rely on compiler
+#         cons = eval_g.insertConstant(False)
+#         ge = eval_g.findNode("aten::ge")
+#         ge_inps = list(ge.inputs())
+#         assert ge_inps[1].toIValue() == 200
+#         assert ge_inps[0].node().kind() == "aten::__getitem__"
+#         cons.node().moveBefore(ge)
+#         ge.replaceAllUsesWith(cons.node())
+#         torch._C._jit_pass_constant_propagation(eval_g)
+#
+#     eval_g.makeMultiOutputIntoTuple()
+#     func = torch._C._create_function_from_graph("partial_eval_graph", eval_g)
+#     # print(func.code)
+#     # print(eval_g)
+#     [node.destroy() for node in eval_g.findAllNodes("prim::RaiseException")]
+#     torch._C._jit_pass_dce(eval_g)
+#     # print(eval_g)
+#     # print(mapping)
+#     # TODO: assumption that the input(1) size < 200
+#     # gets clean graph
+#     return compute, eval_g, mapping, froze
