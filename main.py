6975def53e139dc2d0f4812547474e3404ba8b20
Maksim Levental <mlevental@fb.com>
2021-12-04 02:11:41 -0600

    params not needed

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ from memory_observer import (
     parse_ops_dict,
     with_log,
 )
-
 from strategies import (
     greedy_by_size,
     bump_allocator,
@@ -24,7 +23,7 @@ from strategies import (
     verify_allocation,
     PlannedAlloc,
     mincost_flow,
-    gergov,
+    gergov, make_df_from_reqs,
 )
 
 
@@ -105,19 +104,20 @@ def maybe_make_dir(dir):
         os.mkdir(dir)
 
 
-def make_planned_allocs_csv(model_name, params):
-    ops_dict = load_ops_yaml(f"{model_name}.{params}")
+def make_planned_allocs_csv(model_name, params=None):
+    ops_dict = load_ops_yaml(f"{model_name}.{params}" if params is not None else f"{model_name}")
     ops_dict = parse_ops_dict(ops_dict)
     _, _, names_to_allocs = get_call_chains(ops_dict)
     lvr_to_req_plus_root_caller = export_memory_reqs(ops_dict, names_to_allocs)
     req_allocs = [req for _lvr, (req, _fn_name) in lvr_to_req_plus_root_caller.items()]
-    print(model_name, "num allocs", len(req_allocs), flush=True)
+    req_allocs_df = make_df_from_reqs(req_allocs)
+    print(model_name, "num allocs", len(req_allocs_df), flush=True)
 
     strats = []
-    strats.append(plan_greedy_strats(req_allocs, model_name))
-    if len(req_allocs) < 10000:
-        strats.append(plan_programming_starts(req_allocs, model_name))
-    strats.append(plan_other_strats(req_allocs, model_name))
+    strats.append(plan_greedy_strats(req_allocs_df, model_name))
+    if len(req_allocs_df) < 10000:
+        strats.append(plan_programming_starts(req_allocs_df, model_name))
+    strats.append(plan_other_strats(req_allocs_df, model_name))
 
     for strat, plan in chain(*[s.items() for s in strats]):
         plan: List[PlannedAlloc]
@@ -126,7 +126,11 @@ def make_planned_allocs_csv(model_name, params):
             print(f"{model_name} {strat} memory plan not valid")
         else:
             maybe_make_dir(f"planned_allocs/{model_name}")
-            with open(f"planned_allocs/{model_name}/{strat}.{params}.csv", "w") as f:
+            if params is not None:
+                fp = f"planned_allocs/{model_name}/{strat}.{params}.csv"
+            else:
+                fp = f"planned_allocs/{model_name}/{strat}.csv"
+            with open(fp, "w") as f:
                 for p_alloc in plan:
                     root_caller = lvr_to_req_plus_root_caller[
                         p_alloc.lvr
